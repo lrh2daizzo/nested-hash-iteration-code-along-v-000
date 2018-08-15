51f661b5f04ed8fbfe8ad1eaeb0cc7879645b5f3
Luis Herrera <herrera.lrh@gmail.com>
2018-08-15 19:31:09 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each {|person, contact_details_hash|
     contact_details_hash.each {|attribute, data|
+      if attribute == :favorite_ice_cream_flavors
     binding.pry}
   }
 
